38ade1598f893589ccfad9092ce279aa90064005
coder-joe-smoe <dtpham17@gmail.com>
2022-03-10 01:01:08 -0800

    Update index.js

--- helloWorld/src/index.js
+++ helloWorld/src/index.js
@@ -8,6 +8,7 @@ import react from 'react'
 
 function tick() {
   const element = (
+
     <div>
       <h1>Hello, world!</h1>
       <h2>It is {new Date().toLocaleTimeString()}.</h2>
@@ -19,6 +20,7 @@ function tick() {
       Web Developer
     </p>
     <button>Learn More</button>
+    <button>Book Now</button>
 
     <h3>About</h3>
     <p>This is where I'm going say my strengths</p>
@@ -36,7 +38,41 @@ function tick() {
       <li></li>
     </ul>
 
-<h3></h3>
+    <h3>Projects</h3>
+    <p></p>
+    <img src='https://www.itmountain.com'/>
+    <img src='/'/>
+    <img src='/'/>
+    <br />
+    <img src='/'/>
+    <img src='/'/>
+    <img src='/'/>
+
+<h3>Testimonials</h3>
+<p></p>
+
+
+
+<footer>
+<ul className="col-4-md">
+<li>Home</li>
+<li>About</li>
+<li>Services</li>
+<li>Projects</li>
+<li>Testimonials</li>
+</ul>
+
+<ul className="col-4-md">
+<li>Contact</li>
+<li>email: dtpham17@gmail.com</li>
+<li>phone: 832-495-6966</li>
+<li>fill out form</li>
+
+</ul>
+
+
+</footer>
+
 
     </div>
 
@@ -46,4 +82,4 @@ function tick() {
   ReactDOM.render(element, document.getElementById('root'));
 }
 
-setInterval(tick, 1000);
+setInterval(tick, 3000);
