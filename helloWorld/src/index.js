36f098a9a5be27090f586a94da40b0e46dbb66c1
coder-joe-smoe <dtpham17@gmail.com>
2022-03-20 22:18:30 -0700

    styling hero area

--- helloWorld/src/index.js
+++ helloWorld/src/index.js
@@ -1,5 +1,9 @@
 import ReactDOM from 'react-dom'
 import react from 'react'
+import './/components/bootstrap.css'
+// import 'Sans-serif';
+import 'sans-serif/src/sans-serif.css';
+import './index.css'
 
 // const element = <h1>Hello, world</h1>;
 // ReactDOM.render(element, document.getElementById('root'));
@@ -8,19 +12,27 @@ import react from 'react'
 
 function tick() {
   const element = (
-
     <div>
-      <h1>Hello, world!</h1>
-      <h2>It is {new Date().toLocaleTimeString()}.</h2>
-    
-    <h3>
-      I am Devin Pham
-    </h3>
-    <p>
-      Web Developer
-    </p>
-    <button>Learn More</button>
-    <button>Book Now</button>
+    <div className="navbar">
+      <h1>Logo - Nav</h1>
+    </div>
+
+<div className="hero__container">
+    <div className="hero__text">
+      <h2 className="hero__title">
+            I am Devin Pham
+      </h2>
+      <p className="hero__desc">
+            Web Developer
+      </p>
+    </div>
+    <button className="hero__btn">Book Now</button>
+
+<img src="/" alt="" />
+
+</div>
+
+
 
     <h3>About</h3>
     <p>This is where I'm going say my strengths</p>
@@ -40,13 +52,13 @@ function tick() {
 
     <h3>Projects</h3>
     <p></p>
-    <img src='https://www.itmountain.com'/>
-    <img src='/'/>
-    <img src='/'/>
+    <img src='https://www.itmountain.com' alt=""/>
+    <img src='/' alt="" />
+    <img src='/' alt="" />
     <br />
-    <img src='/'/>
-    <img src='/'/>
-    <img src='/'/>
+    <img src='/' alt="" />
+    <img src='/' alt="" />
+    <img src='/' alt="" />
 
 <h3>Testimonials</h3>
 <p></p>
