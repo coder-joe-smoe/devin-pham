e09dd765699237711620623e9901997f2019e98c
coder-joe-smoe <dtpham17@gmail.com>
2022-04-02 10:01:50 -0700

    working on about us

--- helloWorld/src/components/about/about.js
+++ helloWorld/src/components/about/about.js
@@ -9,14 +9,36 @@ function About() {
 <h3>About
 <AiOutlineGlobal />
 </h3>
-<p>This is where I'm going say my strengths</p>
-<ul>
-<li>web development</li>
-<li>it</li>
-<li>creating efficient systems</li>
-<li>writing - technical writing, copywriting</li>
-</ul>
+<div className="about__item">
 </div>
+<div className="about__text">
+  <h4 className="about__title">web development</h4>
+  <p className="about__desc"></p>
+</div>
+<div className="about__item">
+</div>
+<div className="about__text">
+  <h4 className="about__title">it</h4>
+  <p className="about__desc"></p>
+</div>
+<div className="about__item">
+</div>
+
+<div className="about__text">
+  <h4 className="about__title">creating efficient systems</h4>
+  <p className="about__desc"></p>
+  <div className="about__item">
+  </div>
+</div>
+  <div className="about__text">
+  <h4 className="about__title">writing - technical writing, copywriting</h4>
+  <p className="about__desc"></p>
+  <div className="about__item">
+  </div> 
+</div>
+</div>
+
+
   )
 }
 
